11bb8b6c217a561e66daee171dc2f6dca8f26c7d
PiggySpeed <jlee14c@gmail.com>
2017-02-01 10:53:10 -0800

    minor

--- src/app/app.js
+++ src/app/app.js
@@ -10,15 +10,43 @@ let app = () => {
 };
 
 class AppCtrl {
-  constructor() {
-    this.bear = 'bear'
+  constructor($scope) {
+    var x = 'firee';
+    this.bear = 'bear';
+    this.bunnies = 'bunnies!';
+    $scope.tree = "HAY HAY";
+    $scope.bunnies = "these are bunnies: " + this.bunnies;
+    $scope.calculateTree = this.calculateTree;
+    $scope.calculateAnimals = {
+      bunnies: this.calculateBunnies.bind(this, x)
+    }
   }
+  calculateTree(value) {
+    return (value * 2) + ' trees';
+  }
+  calculateBunnies(value, x) {
+    console.log(x);
+    return arguments.length
+      ? ('bunnnnnnies!')
+      : ('aewf');
+  }
+}
+
+class JungleController {
+  constructor($scope) {
+    $scope = {
+      monkeys: 100,
+      lions: 20
+    };
+  }
+
 }
 
 const MODULE_NAME = 'app';
 
 angular.module(MODULE_NAME, [])
   .directive('app', app)
-  .controller('AppCtrl', AppCtrl);
+  .controller('AppCtrl', AppCtrl)
+  .controller('JungleController', JungleController);
 
 export default MODULE_NAME;
